993bdc0951da293a85135bb87993691a953248d8
tianlinchun <260628258@qq.com>
2017-04-19 10:50:30 +0800

    frist commmit

---
+++ CocoaPodsViewTest/CocoaPodsViewTest/CocoaPodsViewTest/CocoaPodsViewTest.swift
@@ -0,0 +1,23 @@
+//
+//  CocoaPodsViewTest.swift
+//  CocoaPodsViewTest
+//
+//  Created by feng on 2017/4/19.
+//  Copyright © 2017年 tianlinchun. All rights reserved.
+///Users/tlcdev/Desktop/CocoaPodsViewTest/CocoaPodsViewTest/CocoaPodsViewTest/CocoaPodsViewTest
+
+import UIKit
+
+class CocoaPodsViewTest: NSObject {
+    
+    static let test = CocoaPodsViewTest()
+    private override init() {
+        super.init()
+        
+    }
+    
+    public func hahha()
+    {
+        print("哈哈哈哈，大笑")
+    }
+}
---
+++ CocoaPodsViewTest/CocoaPodsViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPodsViewTest
+//
+//  Created by feng on 2017/4/19.
+//  Copyright © 2017年 tianlinchun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
